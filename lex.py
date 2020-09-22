f498e8de1069e48957e67ce81f8f9930b596dbb5
Bruno Henrique Glowaski Morais <bruno.glowaski@outlook.com>
2020-09-21 22:11:50 -0400

    Ajustar identificação dos casamentos mais longos

--- lex.py
+++ lex.py
@@ -1,12 +1,12 @@
-import re
+import regex
 
 def lex(content: str):
     line_n = 1
     result = list()
 
     # Build match patterns.
-    pattern = re.compile(r"""
-    (?P<whitespace>\s)
+    pattern = regex.compile(r"""
+    (?p)(?P<whitespace>\s)
     |(?P<if>if)
     |(?P<else>else)
     |(?P<int>int)
@@ -16,7 +16,7 @@ def lex(content: str):
     |(?P<operator>[\+\-\*/<{<=}>{>=}{==}{!=}=;,\(\)\{\}{\\\*}{/\*}])
     |(?P<ID>[a-z]+)
     |(?P<NUM>[0-9]+)
-    """, re.VERBOSE)
+    """, regex.VERBOSE)
 
     # Split file contents by its lines.
     lines = content.splitlines()
@@ -25,21 +25,26 @@ def lex(content: str):
     for line in lines:
         column_n = 1
         while(column_n <= len(line)):
-            # Find longest match.
+            # Find the longest match.
             match = pattern.match(line, column_n - 1)
 
-            # If no match was found, next character must be invalid
+            # If no match was found, next character must be invalid. Raise a error message and move towards the next character.
             if not match:
                 print("Error: invalid character at line ", line_n, ", column ", column_n, ".", sep = '')
                 column_n += 1
+                continue
+            # If whitespace was found, ignore it.
+            elif match.lastgroup == "whitespace":
+                pass
+            # If an operator was found, add the identified operator into the list.
+            elif match.lastgroup == "operator":
+                result.append(match.group())
+            # Otherwise, add the capture group name into the list.
             else:
-                if match.lastgroup == "whitespace":
-                    pass
-                elif match.lastgroup == "operator":
-                    result.append(match.group())
-                else:
-                    result.append(match.lastgroup)
-                column_n += len(match.group())
+                result.append(match.lastgroup)
+
+            # Advance the stream towards the character after the matched token.
+            column_n += len(match.group())
 
         line_n += 1
     return result
