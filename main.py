ce678752069f9b60c56787b09e7e14b21bc8b731
Bruno Henrique Glowaski Morais <bruno.glowaski@outlook.com>
2020-09-23 16:14:03 -0400

    Aplicar a função lex na main

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+from lex import lex
 
 f = open('exemplo.txt', 'r')
 stext = f.read()
-print(stext)
+tokens = lex(stext)
+
+if len(tokens) > 0:
+    for token in tokens[0:-1]:
+        print(token.type, sep='', end=", ")
+    print(tokens[-1].type)
